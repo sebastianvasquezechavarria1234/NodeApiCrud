b8221f0878bcdefdf28b720b473a902d9464a416
sebastianvasquezechavarria1234 <sebasvasquez1314@gmail.com>
2026-04-17 12:17:27 -0500

    feat: enable CORS support

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import autoRoutes from "./src/routes/auto.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 
 // Routes
